3d962da37ef5b0835da2a7ad6e54f6ae03563c1a
Minajolie <fayeamina303@gmail.com>
2024-07-04 10:12:00 -0700

    ajout des nouveaux fichiers

---
+++ fiche.js
@@ -0,0 +1,16 @@
+let x = parseInt(prompt("Quel est ton age"));
+if (x<=14){
+    alert("vous etes un enfant")
+}
+else if (x<=24){
+    alert("vous etes un adolescent")
+}
+else if (x<=64){
+    alert("vous etes un adulte")
+}
+else if (x>=65){
+    alert("vous etes un aine")
+}
+else{
+    alert("vous n'avez pas mis votre age")
+}
